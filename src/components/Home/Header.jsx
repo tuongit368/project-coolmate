1115868f665c13fb80ff5e88a13751cae4385f1b
TuongDay <tuongit368@gmail.com>
2024-04-04 17:16:45 +0700

    Update code giao dien Header

---
+++ src/components/Home/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import logoHome from "../../assets/images/logoHome.png"
+import "../../styles/home/header.css"
+
+import { CiSearch } from "react-icons/ci";
+import { FaUser } from "react-icons/fa";
+import { IoBagHandleSharp } from "react-icons/io5";
+
+function Header() {
+  return (
+    <div className='header-container'>
+      <img className='header-logo' src={ logoHome } alt="logoHome" />
+      <ul className='header-nav-links'>
+        <li>Sale</li>
+        <li>Sản phẩm</li>
+        <li>Đồ lót</li>
+        <li>Đồ thể thao</li>
+        <li>Mặc hàng ngày</li>
+        <li>Nước hoa</li>
+        <li>Sản xuất riêng</li>
+        <li>Care & share</li>
+      </ul>
+      <div className='header-actionUser'>
+        <div className='header-actionUser-search'>
+
+          <CiSearch />
+          <input type="text" placeholder='Tìm kiếm sản phẩm...'/>
+
+          </div>
+
+          <FaUser className='header-actionUser-userIcon'/>
+          <IoBagHandleSharp className='header-actionUser-bagIcon' />
+      </div>
+    </div>
+  )
+}
+
+export default Header
